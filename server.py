ddea2abbc820e2789dd0017baca5dee615755207
Kristin Germeroth <kristin.germeroth@gmail.com>
2020-01-12 16:17:33 -0800

    enabled debug for automatic updates

--- server.py
+++ server.py
@@ -2,6 +2,7 @@
 from jinja2 import StrictUndefined
 from flask import Flask, render_template, request, redirect, flash, session, jsonify, Markup
 from flask_debugtoolbar import DebugToolbarExtension
+import os
 
 app = Flask(__name__)
 
@@ -21,12 +22,10 @@ def show_login_form():
 
 if __name__ == '__main__':
 
-    # app.debug = True
+    app.debug = True
 
     # make sure templates, etc. are not cached in debug mode
-    # app.jinja_env.auto_reload = app.debug
-
-    connect_to_db(app)
+    app.jinja_env.auto_reload = app.debug
 
     # Use the DebugToolbar
     # DebugToolbarExtension(app)
